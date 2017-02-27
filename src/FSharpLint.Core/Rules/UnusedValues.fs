65ed52a71340d47e787466fdc6c10b6b30433f22
Matt <matt@mattmcveigh.co.uk>
2017-02-27 23:55:33 +0000

    Added failing test and scaffolding for new analyser

---
+++ src/FSharpLint.Core/Rules/UnusedValues.fs
@@ -0,0 +1,27 @@
+﻿namespace FSharpLint.Rules
+
+module UnusedValues =
+    
+    open Microsoft.FSharp.Compiler.Ast
+    open Microsoft.FSharp.Compiler.SourceCodeServices
+    open FSharpLint.Framework
+    open FSharpLint.Framework.Analyser
+    open FSharpLint.Framework.Ast
+    open FSharpLint.Framework.Configuration
+
+    [<Literal>]
+    let AnalyserName = "UnusedValues"
+
+    let analyser (args: AnalyserArgs) : unit = 
+        let syntaxArray, skipArray = args.SyntaxArray, args.SkipArray
+
+        let isNotSuppressed i ruleName =
+            AbstractSyntaxArray.getSuppressMessageAttributes syntaxArray skipArray i 
+            |> AbstractSyntaxArray.isRuleSuppressed AnalyserName ruleName
+            |> not
+
+        for i = 0 to syntaxArray.Length - 1 do
+            match syntaxArray.[i].Actual with
+            | AstNode.Binding(SynBinding.Binding(_, _, _, _, _, _, _, pattern, _, _, _, _)) ->
+                ()
+            | _ -> ()
---
+++ tests/FSharpLint.Core.Tests/Rules/TestUnusedValues.fs
@@ -0,0 +1,37 @@
+﻿module TestUnusedValues
+
+open NUnit.Framework
+open FSharpLint.Rules.UnusedValues
+open FSharpLint.Framework.Configuration
+
+let config = 
+    let ruleEnabled = { Rule.Settings = Map.ofList [ ("Enabled", Enabled(true)) ] }
+
+    Map.ofList 
+        [ (AnalyserName, { Rules = Map.empty; Settings = Map.ofList [ ("Enabled", Enabled(true)) ] }) ]
+              
+[<TestFixture>]
+type TestUnusedValues() =
+    inherit TestRuleBase.TestRuleBase(analyser, config)
+
+    [<Test>]
+    member this.``Suggestion when local binding unused.``() = 
+        this.Parse("""
+module Program
+
+let foo () = 
+    let bar = 1 + 1
+    ()""")
+
+        Assert.IsTrue(this.ErrorExistsAt(4, 8))
+
+    [<Test>]
+    member this.``No suggestion when local binding is used.``() = 
+        this.Parse("""
+module Program
+
+let foo () = 
+    let bar = 1 + 1
+    bar""")
+
+        this.AssertNoWarnings()
